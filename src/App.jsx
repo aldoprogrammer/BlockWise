fd386dec0edd5b889f3aa93d553b302f616e35f8
aldoprogrammer <aldobesma@gmail.com>
2024-06-06 07:36:21 +0700

    refactor: Remove unused routes and components

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Login } from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import Medichine from './pages/Medichine';
 import PrintInvoice from './pages/PrintInvoice';
-import DomainInsight from './pages/DomainInsight';
 import Profile from './pages/Profile';
 import Transactions from './pages/Transactions';
 import Leaderboard from './pages/Leaderboard';
@@ -15,8 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/medicine" element={<Medichine />} />
-        <Route path='/domain-insight' element={<DomainInsight />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/transactions' element={<Transactions />} />
         <Route path='/leaderboard' element={<Leaderboard />} />
